0f13e0f87a2208a1081e0caaa42710c1a07772f8
mpikuliak <matus.pikuliak@gmail.com>
2019-12-03 09:24:54 +0100

    Day 3 done

---
+++ 03/01.py
@@ -0,0 +1,71 @@
+import itertools
+
+instructions = [
+    [
+        (point[0], int(point[1:]))
+        for point
+        in line.split(',')
+    ]
+    for line
+    in open('input').readlines()
+]
+
+
+def compute_points(instructions):
+
+    new_point = {
+        'U': lambda x, y, l: (x + l, y),
+        'D': lambda x, y, l: (x - l, y),
+        'R': lambda x, y, l: (x, y + l),
+        'L': lambda x, y, l: (x, y - l),
+    }
+
+    points = [(0, 0)]
+    for direction, length in instructions:
+        points.append(
+            new_point[direction](*points[-1], length)
+        )
+    return points
+
+points = [
+    compute_points(i)
+    for i
+    in instructions
+]
+
+
+def cross(a_1, a_2, b_1, b_2):
+    if a_1 == b_1 or a_1 == b_2:
+        return a_1
+    if a_2 == b_1 or a_2 == b_2:
+        return a_2
+    if a_1[0] == a_2[0] and b_1[0] == b_2[0]:
+        return None
+    if a_1[1] == a_2[1] and b_1[1] == b_2[1]:
+        return None
+    if a_1[0] == a_2[0]:
+        return cross(b_1, b_2, a_1, a_2)
+    if a_1[0] > a_2[0]:
+        return cross(a_2, a_1, b_1, b_2)
+    if b_1[1] > b_2[1]:
+        return cross(a_1, a_2, b_2, b_1)
+    if a_1[0] < b_1[0] < a_2[0] and b_1[1] < a_1[1] < b_2[1]:
+        return b_1[0], a_1[1]
+    return None
+
+
+def distance(a_1, a_2, b_1, b_2):
+    c = cross(a_1, a_2, b_1, b_2)
+    if c is None:
+        return 0
+    return abs(c[0]) + abs(c[1])
+
+
+num_lines = len(points[0]) - 1
+min_distance = 10e10
+for i, j in itertools.product(range(num_lines), range(num_lines)):
+    dist = distance(*points[0][i: i + 2], *points[1][j: j + 2])
+    if 0 < dist < min_distance:
+        min_distance = dist
+
+print(min_distance)
