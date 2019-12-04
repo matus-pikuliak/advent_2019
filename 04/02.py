f971b1b7dc1b2db8e9aac14d8439094055543bb0
matus-pikuliak <matus.pikuliak@stuba.sk>
2019-12-04 12:26:49 +0100

    more elegant 04.02

--- 04/02.py
+++ 04/02.py
@@ -3,7 +3,7 @@ for i in range(402328, 864247 + 1):
     num = str(i)
     if sorted(num) != list(num):
         continue
-    if any(num[j] == num[j+1] and (j+2 == 6 or num[j+2] != num[j]) and (j-1 == -1 or num[j-1] != num[j]) for j in range(5)):
+    if any(num.count(num[j]) == 2 for j in range(5)):
         total += 1
 print(total)
 
