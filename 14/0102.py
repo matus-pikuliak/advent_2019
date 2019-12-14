ca19b4a25937296919a50015581280ad257ec8e2
mpikuliak <matus.pikuliak@gmail.com>
2019-12-14 11:52:43 +0100

    Less verbose create function

--- 14/0102.py
+++ 14/0102.py
@@ -1,59 +1,52 @@
 import math
 
-requirements = {}
+reactions = {}
 for line in open('input'):
-    req, out = line.strip().split('=>')
-    req = [
+    required, target = line.strip().split('=>')
+    required = [
         r.split()
         for r
-        in req.split(', ')
+        in required.split(', ')
     ]
-    count, chem = out.split()
-    requirements[chem] = {
-        'count': int(count),
+    target_count, target = target.split()
+    reactions[target] = {
+        'count': int(target_count),
         'req': {
             chem: int(count)
             for count, chem
-            in req
+            in required
         }
     }
 
 
-def create(count, chemical, leftovers):
+def create(target_count, target, leftovers):
 
-    reaction_count = math.ceil(count / requirements[chemical]['count'])
+    if target_count == 0:
+        return 0, leftovers
 
-    req = {
-        req_chem: req_count * reaction_count
-        for req_chem, req_count
-        in requirements[chemical]['req'].items()
-    }
+    reaction_count = math.ceil(target_count / reactions[target]['count'])
 
-    for left_chem, left_count in list(leftovers.items()):
-        if left_chem in req:
-            if req[left_chem] > left_count:
-                req[left_chem] -= left_count
-                del leftovers[left_chem]
-            elif req[left_chem] == left_chem:
-                del req[left_chem]
-                del leftovers[left_chem]
-            else:
-                leftovers[left_chem] -= req[left_chem]
-                del req[left_chem]
+    required = dict(reactions[target]['req'])
+    for chem in required:
+        required[chem] *= reaction_count
+
+    for chem, count in leftovers.items():
+        if chem in required:
+            count = min(required[chem], count)
+            required[chem] -= count
+            leftovers[chem] -= count
 
     ore_sum = 0
-    for req_chem, req_count in req.items():
-        if req_chem == 'ORE':
-            ore_sum += req_count
+    for chem, count in required.items():
+        if chem == 'ORE':
+            ore_sum += count
         else:
-            ore_count, leftovers = create(req_count, req_chem, leftovers)
+            ore_count, leftovers = create(count, chem, leftovers)
             ore_sum += ore_count
 
-    left_target = reaction_count * requirements[chemical]['count'] - count
-    if left_target > 0:
-        if chemical not in leftovers:
-            leftovers[chemical] = 0
-        leftovers[chemical] += left_target
+    if target not in leftovers:
+        leftovers[target] = 0
+    leftovers[target] += reaction_count * reactions[target]['count'] - target_count
 
     return ore_sum, leftovers
 
